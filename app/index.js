94f45083b0ef73cbe3add9e8eb497a6a79c9acb4
Arthur Craveiro <arthur.craveiro@gmail.com>
2018-03-26 15:07:07 -0300

    TextInput

--- app/index.js
+++ app/index.js
@@ -3,9 +3,13 @@ import EStyleSheet from 'react-native-extended-stylesheet';
 
 import Home from './screens/Home';
 
-EStyleSheet.build ({
+EStyleSheet.build({
   $primaryBlue: '#4F6D7A',
   $white: '#FFFFFF',
+  $lightGray: '#F0F0F0',
+  $border: '#979797',
+  $inputText: '#797979',
 });
 
 export default () => <Home />;
+
